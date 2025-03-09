57e7c7741fc2abdbc482cff850810ddf9301d9ef
varunTyagarayanG <varunmuralidhar9@gmail.com>
2025-03-09 19:32:35 +0530

    initial push

---
+++ config/asgi.py
@@ -0,0 +1,16 @@
+import os
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+
+import chat_app.ws_urls
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(chat_app.ws_urls.websocket_urlpatterns)
+    )
+})
