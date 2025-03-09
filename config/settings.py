9f558c6167951b32b7cd597cd43f452fc31f127b
varunTyagarayanG <varunmuralidhar9@gmail.com>
2025-03-10 00:34:15 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == 'True'  
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')  
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'baldies.onrender.com').split(',') 
 
 INSTALLED_APPS = [
     'django.contrib.admin',
