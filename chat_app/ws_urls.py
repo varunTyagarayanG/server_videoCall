ae8c44b7a1c5e80149660f229daa8a18413fafc5
varunTyagarayanG <varunmuralidhar9@gmail.com>
2025-03-10 02:20:45 +0530

    WSS-UPDATE

--- chat_app/ws_urls.py
+++ chat_app/ws_urls.py
@@ -4,6 +4,6 @@ from chat_app.consumers.message import MessageConsumer
 from chat_app.consumers.notification import NewUserConsumer
 
 websocket_urlpatterns = [
-    path('ws/notification/', NewUserConsumer.as_asgi()),
-    path('ws/message/<str:username>/', MessageConsumer.as_asgi())
+    path('/ws/notification/', NewUserConsumer.as_asgi()),
+    path('/ws/message/<str:username>/', MessageConsumer.as_asgi())
 ]
