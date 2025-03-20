e6b2b2277506701458c76ee4d04ef4d9286eac60
varunTyagarayanG <varunmuralidhar9@gmail.com>
2025-03-20 06:15:03 +0530

    ok

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == 'True'  
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'baldies.onrender.com').split(',') 
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'daphne',
